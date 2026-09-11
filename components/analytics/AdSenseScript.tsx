ae36f0a8cad1cbe368c3e3430233dbba42287876
Tawfik Mklada <tawfik164@gmail.com>
2026-09-12 00:47:50 +0200

    adsense: load script in <head> for verification

--- components/analytics/AdSenseScript.tsx
+++ components/analytics/AdSenseScript.tsx
@@ -12,7 +12,7 @@ export default function AdSenseScript() {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
+      strategy="beforeInteractive"
     />
   );
 }
